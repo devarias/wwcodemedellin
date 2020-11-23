24ed7d021c4af3cd88e597fabb9e94f76db043fa
David Dev Arias <gedafu2005@gmail.com>
2020-11-22 19:55:32 -0500

    Bubble Sort en JavaScript

---
+++ buble.js
@@ -0,0 +1,31 @@
+/**
+ * @param {*} array array of integers
+ */
+const bubleSort = (array) => {
+    let arraySort = array
+    let tmp;
+    let i = 0, j = 1;
+    let size = array.length
+
+	if ( size <= 0)
+        return "El array está vacio!";
+    if ( size == 1)
+    return "El array solo tiene un elemento!";
+	if (size > 1)
+	{
+		for (; i < size - 1; i++, j++)
+		{
+			for (j = 0; j < size - 1; j++)
+			{
+				if (arraySort[j] > arraySort[j + 1])
+				{
+					tmp = arraySort[j];
+					arraySort[j] = arraySort[j + 1];
+					arraySort[j + 1] = tmp;
+				}
+			}
+		}
+	}
+    return arraySort
+}
+console.log(bubleSort(array))
