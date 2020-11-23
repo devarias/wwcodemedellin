6f198c9583e0b1985a1fc5559c01f17089e2b6e3
David Dev Arias <gedafu2005@gmail.com>
2020-11-23 01:46:45 -0500

    factorial con Recursión en JavaScript

--- factorial.js
+++ factorial.js
@@ -3,13 +3,13 @@
  * @param {*} number Int
  */
 const factorial = (number) => {
-    let resp = 0
+    let resp = 0;
 	if (number < 0)
 		return "El número es negativo!";
 	if (number === 0)
 		return 1;
 	else
         resp += (number * factorial(number - 1));
-    return resp
+    return resp;
 }
 console.log(factorial(x));
